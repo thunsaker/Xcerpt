69f5125f814a9b713bfe70092a9971ad04ff4d4f
baieric <ebai@uwaterloo.ca>
2016-03-09 02:26:50 -0500

    improve Bing Search results :mag:

--- app/src/main/java/com/transcendentlabs/xcerpt/SearchAsyncTask.java
+++ app/src/main/java/com/transcendentlabs/xcerpt/SearchAsyncTask.java
@@ -14,13 +14,21 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 public class SearchAsyncTask extends AsyncTask<Void, Void, Void> {
 
     private final String TAG = getClass().getName();
     private final int MAX_QUERY_LENGTH = 1961;
-
+    String[] common = {"a", "an", "and", "are", "as", "at", "be", "but", "by",
+            "for", "if", "in", "into", "is", "it", "i",
+            "no", "not", "of", "on", "or", "such",
+            "that", "the", "their", "then", "there", "these",
+            "they", "this", "to", "was", "will", "with"};
     private String mSearchStr;
     private int mNumOfResults = 0;
 
@@ -37,10 +45,27 @@ public class SearchAsyncTask extends AsyncTask<Void, Void, Void> {
     @Override
     protected Void doInBackground(Void... params) {
         try {
-            // remove quotation marks and extra whitespace
-            mSearchStr = mSearchStr.replaceAll("\"", "").replaceAll("\\s+", " ");
+            // remove all non-alphabet characters and extra whitespace
+            mSearchStr = mSearchStr.replaceAll("[^\\p{L}0-9 '\\u2019]", " ");
+            String[] words = mSearchStr.toLowerCase().split("\\s+");
+            ArrayList<String> wordList = new ArrayList<>();
+            HashSet<String> commonWords = new HashSet<>(Arrays.asList(common));
+            HashSet<String> alreadyPresent = new HashSet<>();
+            for(String word : words) {
+                if(alreadyPresent.add(word) && !commonWords.contains(word)) {
+                    wordList.add(word);
+                    if(wordList.size() > 32) {
+                        break;
+                    }
+                }
+            }
+            String search = "";
+            for(String word : wordList) {
+                search = search + " " + word;
+            }
+            mSearchStr = search.trim();
             // wrapped with quotation marks for verbatim search
-            mSearchStr = "\"" + mSearchStr +  "\"";
+            // String verbatim = "\"" + mSearchStr +  "\"";
 
             int numSpaces = mSearchStr.length() - mSearchStr.replaceAll(" ", "").length();
             int less = 10;
@@ -54,12 +79,13 @@ public class SearchAsyncTask extends AsyncTask<Void, Void, Void> {
                 }else {
                     subSearch = subSearch.substring(0, lastSpace);
                     numSpaces = subSearch.length() - subSearch.replaceAll(" ", "").length();
+                    // verbatim = "\"" + subSearch + "\"";
                     encodedLength = subSearch.length() + numSpaces * 2;
                     less = less * 2;
                     Log.e("temp", subSearch);
                 }
             }
-
+            Log.e("search query", subSearch);
             String searchStr = URLEncoder.encode(subSearch);
             Log.e("temp", searchStr);
             String numOfResultsStr = mNumOfResults <= 0 ? "" : "&$top=" + mNumOfResults;
