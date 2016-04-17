0881b733b5acabc0771a15957529039e44914b7b
baieric <ebai@uwaterloo.ca>
2016-04-16 23:43:52 -0400

    fixed untracked files

--- cropper/build/generated/source/r/release/com/edmodo/cropper/R.java
+++ cropper/build/generated/source/r/release/com/edmodo/cropper/R.java
@@ -51,34 +51,34 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static int imageResource=0x7f010004;
     }
     public static final class color {
-        public static int black_translucent=0x7f030000;
+        public static int black_translucent=0x7f040000;
     }
     public static final class id {
-        public static int CropOverlayView=0x7f040004;
-        public static int ImageView_image=0x7f040003;
-        public static int off=0x7f040002;
-        public static int on=0x7f040000;
-        public static int onTouch=0x7f040001;
+        public static int CropOverlayView=0x7f050004;
+        public static int ImageView_image=0x7f050003;
+        public static int off=0x7f050002;
+        public static int on=0x7f050000;
+        public static int onTouch=0x7f050001;
     }
     public static final class layout {
         public static int crop_image_view=0x7f020000;
     }
     public static final class string {
-        public static int app_name=0x7f050000;
+        public static int app_name=0x7f060000;
     }
     public static final class style {
-        /** 
+        /**  API 11 theme customizations can go here. 
+ API 14 theme customizations can go here. 
+
             Theme customizations available in newer API levels can go in
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
         
- API 11 theme customizations can go here. 
- API 14 theme customizations can go here. 
          */
-        public static int AppBaseTheme=0x7f060000;
+        public static int AppBaseTheme=0x7f030000;
         /**  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static int AppTheme=0x7f060001;
+        public static int AppTheme=0x7f030001;
     }
     public static final class styleable {
         /** Attributes that can be used with a CropImageView.
